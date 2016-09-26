95ae9dab4ae0fb2cf2b1870624965e583318eb43
zoilovely123 <trantram96@gmail.com>
2016-09-26 08:30:48 +0700

    create Element

---
+++ BT2/BT2/Element.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BT2
+{
+    class Element<T>
+    {
+        private T data;
+        public T Data
+        {
+            get { return data; }
+            set { data = value; }
+        }
+        private Element<T> next;
+        internal Element<T> Next
+        {
+            get { return next; }
+            set { next = value; }
+        }
+    }
+}

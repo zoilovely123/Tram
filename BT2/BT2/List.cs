b95ed302d3762196d71c2758fc3afd2eba84a341
zoilovely123 <trantram96@gmail.com>
2016-09-26 09:09:06 +0700

    List

---
+++ BT2/BT2/List.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BT2
+{
+    class List<T>
+    {
+        Element<T> head;
+
+        internal Element<T> Head
+        {
+            get { return head; }
+            set { head = value; }
+        }
+        public List()
+        {
+        }
+        public void addfirst(Element<T> e)
+        {
+            e.Next = head;
+            this.head = e;
+        }
+    }
+}
